3399c8fcdbea9eeca997b2bffde5e2cebd5a257e
PESolut <jahaadpetty@pursuit.org>
2023-07-21 14:25:56 -0400

    added sort first number function

--- controllers/firstNumber.js
+++ controllers/firstNumber.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const controller = express.Router()
-const BitManipulationSingleNumber = require ('./firstNumberUtil.js')
+// const { BitManipulationSingleNumber, SortingSingleNumber } = require('./firstNumberUtil.js');
+const BitManipulationSingleNumber = require('./firstNumberUtil.js')
+const SortingSingleNumber = require('./firstNumberUtil.js')
+
+
 
 controller.get('/', (req, res) => {
     try {
@@ -9,6 +13,9 @@ controller.get('/', (req, res) => {
         if(req.query.list){
             const listOfNumbersString = (req.query.list);
 
+            // check that the query string is a formated like so
+            // [ number1, number2, number3]
+
             // convert the string into an array of numbers
             const listOfNumbersArr = JSON.parse(listOfNumbersString)
             console.log(listOfNumbersArr)
@@ -16,11 +23,17 @@ controller.get('/', (req, res) => {
 
             // peform function on arr
             const bmOutput = BitManipulationSingleNumber(listOfNumbersArr)
+            const sOutput = SortingSingleNumber(listOfNumbersArr)
     
             // output
             res.json({
-                'bit manipulation output': bmOutput
+                'bit manipulation output': bmOutput,
+                'sorting output': sOutput
             })
+        } else {
+            throw {
+                message: 'must include a list query with an array of numbers!', status: 'error'
+            }
         }
     } catch (error){
         res.status(500).json ({ error : error.message })
--- controllers/firstNumberUtil.js
+++ controllers/firstNumberUtil.js
@@ -7,4 +7,22 @@ var BitManipulationSingleNumber = function(n) {
 return r
 };
 
+var SortingSingleNumber = function(n) {
+    // sort the numbers
+    n.sort()
+    // loop through the numbers
+    for(let i = 0; i < n.length; i++){
+        let cN = n[i]
+        let nN = n[i+1]
+        // check if current element number is equal to the next element number
+        // if so, skip next element and continue loop
+        // if not, return current element as that is the lone digit
+        if(cN == nN){
+            i++
+        } else {
+            return n[i]
+        }
+    }
+};
+
 module.exports = BitManipulationSingleNumber
